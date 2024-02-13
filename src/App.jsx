7b9aa68d0fa62319b98efc0dd35fe38229821bca
Hanieh Gharaee <154983983+HaniehGharaee@users.noreply.github.com>
2024-02-13 23:29:40 +0330

    Fix App.jsx project

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import "./main.css";
 import { ConfigProvider } from "antd";
 import Navbar from "./container/Navbar";
 import { Provider as ReduxProvider } from "react-redux";
-import store from "./redux/store";
+//import store from "./redux/store";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FarAway from "./container/FarAway.jsx";
 function App() {
 
     return (
        <>
-            <ReduxProvider store={store}>
+          {/*<ReduxProvider store={store}> */}
                 <ConfigProvider>
                     <Router>
                         <Navbar />
@@ -19,7 +19,7 @@ function App() {
                             </Routes>
                     </Router>
                 </ConfigProvider>
-            </ReduxProvider>
+           {/*</ReduxProvider> */}
             </>
     );
 }
